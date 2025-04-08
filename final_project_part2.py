82d09c03a5a8d7df61474e223d9fe6cf3476f243
Jackqu173 <145812017+Jackqu173@users.noreply.github.com>
2025-04-08 14:53:53 -0700

    Update final_project_part2.py
    small change

--- final_project_part2.py
+++ final_project_part2.py
@@ -12,7 +12,7 @@ def consumerWorker(queue: object, id: int) -> None:
         task_number = task_number - 1
         item = queue.get()
         print(f"[Consumer-{threading.current_thread().name}] Consumed: {item}")
-        time.sleep(random.uniform(0.1, 0.5))  # Simulate variable processing time
+        time.sleep(random.uniform(0.5, 1))  # Simulate variable processing time
         queue.task_done()
 
 def producerWorker(queue: object, task: int) -> None:
@@ -21,7 +21,7 @@ def producerWorker(queue: object, task: int) -> None:
         item = random.randint(0,5)
         queue.put(item)
         print(f"[Producer-{threading.current_thread().name}] Produced: {item}")
-        time.sleep(random.randint(1,2))
+        time.sleep(random.uniform(0.1, 0.2))  # Simulate variable processing time
 
 if __name__ == "__main__":
     buffer = queue.Queue() # buffer to keep track of consumer/producer
