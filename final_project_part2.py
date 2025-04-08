cfb44bdc18f71c69c7a24035bff7d046654d26ff
PolarisVan <dalisioneto@gmail.com>
2025-04-07 22:46:32 -0700

    remove print meant for testing

--- final_project_part2.py
+++ final_project_part2.py
@@ -9,13 +9,11 @@ def consumerWorker(queue: object, id: int):
     """target worker for a consumer thread"""
     global task_number
     while task_number >= id + 1:
-        print(task_number)
         task_number = task_number - 1
         item = queue.get()
         print(f"[Consumer-{threading.current_thread().name}] Consumed: {item}")
         time.sleep(random.uniform(0.1, 0.5))  # Simulate variable processing time
         queue.task_done()
-    print("done")
 
 def producerWorker(queue: object, id: int):
     """target worker for a producer thread"""
