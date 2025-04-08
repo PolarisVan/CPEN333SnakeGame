dc5d3ccf96a0f8f77fe77625ee28551e56eb7e08
Jackqu173 <145812017+Jackqu173@users.noreply.github.com>
2025-04-07 21:32:20 -0700

    Update final_project_part2.py
    the best way i can think off

--- final_project_part2.py
+++ final_project_part2.py
@@ -1,46 +1,48 @@
 # Group #: B21
-# Student names: Peiyu Qu and Dalisio Pereira
+# Student names:
 
 import threading
 import queue
 import time, random
 
 
-def consumerWorker(queue: queue) -> None:
+def consumerWorker(queue: object, id: int):
     """target worker for a consumer thread"""
-    while True:
+    global task_number
+    while task_number >= id + 1:
+        print(task_number)
+        task_number = task_number - 1
         item = queue.get()
-        if item is None:
-            print(f"[Consumer-{threading.current_thread().name}] Exiting.")
-            queue.task_done()
-            break
         print(f"[Consumer-{threading.current_thread().name}] Consumed: {item}")
         time.sleep(random.uniform(0.1, 0.5))  # Simulate variable processing time
         queue.task_done()
+    print("done")
 
-def producerWorker(queue: queue) -> None:
+
+
+
+def producerWorker(queue: object, id: int):
     """target worker for a producer thread"""
     for i in range (5):
         queue.put(random.randint(0,5))
-        time.sleep(random.randint(1,3))
-
-    producer_list.pop(0)
+        time.sleep(random.randint(1,2))
 
 if __name__ == "__main__":
     buffer = queue.Queue()
     num_consumer = 5
     num_producer = 4
     thread_list = []
-    producer_list = []
+    task_number = num_producer * 5
     for i in range(num_consumer):
-        thread_list.append(threading.Thread(target=consumerWorker, args=(buffer,)))
+        thread_list.append(threading.Thread(target=consumerWorker, args=(buffer,i)))
 
     for i in range(num_producer):
-        thread_list.append(threading.Thread(target=producerWorker, args=(buffer,)))
-        producer_list.append(i)
+        thread_list.append(threading.Thread(target=producerWorker, args=(buffer,i)))
 
     for i in thread_list:
         i.start()
 
     for i in thread_list:
         i.join()
+
+    print("done")
