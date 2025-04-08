ab9b5c3f7bead2fa891da56306097d6a7b20b827
Jackqu173 <145812017+Jackqu173@users.noreply.github.com>
2025-04-08 14:44:08 -0700

    Update final_project_part2.py
    add task each producer need to do

--- final_project_part2.py
+++ final_project_part2.py
@@ -7,18 +7,18 @@ import time, random
 
 def consumerWorker(queue: object, id: int) -> None:
     """target worker for a consumer thread"""
-    global task_number # tracker for remaining tasks
+    global task_number
     while task_number >= id + 1:
-        task_number = task_number - 1 # subtracts one task as it is completed
+        task_number = task_number - 1
         item = queue.get()
         print(f"[Consumer-{threading.current_thread().name}] Consumed: {item}")
-        time.sleep(random.uniform(0.1, 0.5))  # simulates variable processing time
+        time.sleep(random.uniform(0.1, 0.5))  # Simulate variable processing time
         queue.task_done()
 
-def producerWorker(queue: object) -> None:
+def producerWorker(queue: object, task: int) -> None:
     """target worker for a producer thread"""
-    for i in range (5):
-        item = random.randint(0,5) # picks a random item to be added to buffer
+    for i in range (task):
+        item = random.randint(0,5)
         queue.put(item)
         print(f"[Producer-{threading.current_thread().name}] Produced: {item}")
         time.sleep(random.randint(1,2))
@@ -27,13 +27,14 @@ if __name__ == "__main__":
     buffer = queue.Queue() # buffer to keep track of consumer/producer
     num_consumer = 5
     num_producer = 4
+    task_per_producer = 6
     thread_list = []
-    task_number = num_producer * 5 # tracker for remaining tasks
+    task_number = num_producer * task_per_producer # tracker for remaining tasks
     for i in range(num_consumer):
         thread_list.append(threading.Thread(target=consumerWorker, args=(buffer,i)))
 
     for i in range(num_producer):
-        thread_list.append(threading.Thread(target=producerWorker, args=(buffer, )))
+        thread_list.append(threading.Thread(target=producerWorker, args=(buffer, task_per_producer)))
 
     for i in thread_list:
         i.start()
